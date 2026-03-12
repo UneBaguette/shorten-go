f15feb2638b121d288cb10d9a93d3c03475c6f45
master@vexahub.com <master@baguette-debian-2gb-nbg1-1>
2026-03-12 16:26:56 +0100

    chore: log ip correctly and added it in CORS

--- cmd/shorten/main.go
+++ cmd/shorten/main.go
@@ -89,6 +89,15 @@ func main() {
 	app.Use(logger.New(logger.Config{
 		Format: "[${time}] ${ip} ${method} ${path} ${status} ${latency}\n",
 		Stream: io.MultiWriter(os.Stdout, logFile),
+    CustomTags: map[string]logger.LogFunc{
+        "ip": func(output logger.Buffer, c fiber.Ctx, data *logger.Data, extraParam string) (int, error) {
+            ip := c.Get("X-Real-IP")
+            if ip == "" {
+                ip = c.IP()
+            }
+            return output.WriteString(ip)
+        },
+    },
 	}))
 
 	app.Use(setupCORS())
@@ -131,6 +140,6 @@ func setupCORS() fiber.Handler {
 	return cors.New(cors.Config{
 		AllowOrigins: strings.Split(os.Getenv("ALLOWED_ORIGINS"), ","),
 		AllowMethods: []string{"GET", "POST", "DELETE"},
-		AllowHeaders: []string{"Content-Type", "X-API-Key", "X-Delete-Token"},
+		AllowHeaders: []string{"Content-Type", "X-API-Key", "X-Delete-Token", "X-Real-IP"},
 	})
 }
